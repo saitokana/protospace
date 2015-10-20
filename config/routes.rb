e8b88aa2ed8691bb9f6150bdee67a43cfd16752e
齊藤可奈 <saitokana@saitoukana-no-MacBook-Pro.local>
2015-10-20 18:20:01 +0900

    デザインの適用 -必要なファイルの取り込み -サーバーを立ち上げるためにDB、コントローラーを作成 -bootstrapのダウンロード・配置

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  resources :prototypes, only: [:index]
+  root to: "prototypes#index"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
