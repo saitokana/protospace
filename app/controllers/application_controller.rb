e2045ec79bb41a87c096bd276e244b8807bb6b5f
齊藤可奈 <saitokana@saitoukana-no-MacBook-Pro.local>
2015-11-03 20:44:38 +0900

    update時のカラムの指定

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,7 +5,8 @@ class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.for(:sign_up){|u| u.permit(:profile, :position, :member, :name, :image, :email, :password, :password_confirmation) }
+    devise_parameter_sanitizer.for(:sign_up){|u| u.permit(:profile, :position, :member, :name, :avatar, :email, :password, :password_confirmation) }
+    devise_parameter_sanitizer.for(:account_update){|u| u.permit(:profile, :position, :member, :name, :avatar, :email, :password, :password_confirmation, :current_password) }
   end
 
 end
