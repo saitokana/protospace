c0efce5a7e6994dcf17a2da732595f518bf38e7b
齊藤可奈 <saitokana@saitoukana-no-MacBook-Pro.local>
2015-11-08 13:18:01 +0900

    prototypeに関連するアクションを設定（index,new,create以外はまだ途中です。）

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,10 +1,31 @@
 class PrototypesController < ApplicationController
 
   def index
+    @prototypes = Prototype.all.page(params[:page]).per(8)
   end
 
   def new
     @prototype = Prototype.new
+    @prototype.captured_images.build
+  end
+
+  def create
+    @prototype = Prototype.create(prototype_params)
+    redirect_to :root and return
+  end
+
+  def edit
+    @prototype = Prototype.find(params[:id])
+  end
+
+  def show
+    @prototype = Prototype.find(params[:id])
+    @user = prototype.user
+  end
+
+  private
+  def prototype_params
+    params.require(:prototype).permit(:catch_copy, :concept, :title, captured_images_attributes: [:id, :name,:role]).merge(user_id: current_user.id)
   end
 
 end
