2fd4ddeb5a9efca9f91d00a41c7f30e625f137c8
齊藤可奈 <saitokana@saitoukana-no-MacBook-Pro.local>
2015-11-08 13:19:21 +0900

    それぞれのモデルに必要な記述を追加

--- app/models/prototype.rb
+++ app/models/prototype.rb
@@ -1,2 +1,6 @@
 class Prototype < ActiveRecord::Base
+  belongs_to :users
+  validates_presence_of :user_id, :title, :catch_copy, :concept
+  has_many :captured_images
+  accepts_nested_attributes_for :captured_images
 end
