072a5ea5a16d98b9715dfbf090a91e8d9d830959
齊藤可奈 <saitokana@saitoukana-no-MacBook-Pro.local>
2015-11-08 13:23:00 +0900

    captured_imagesテーブルの設計

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151102165751) do
+ActiveRecord::Schema.define(version: 20151104044425) do
+
+  create_table "captured_images", force: :cascade do |t|
+    t.text     "name",         limit: 65535
+    t.integer  "role",         limit: 4
+    t.integer  "prototype_id", limit: 4
+    t.datetime "created_at",                 null: false
+    t.datetime "updated_at",                 null: false
+  end
 
   create_table "prototypes", force: :cascade do |t|
     t.string   "catch_copy",     limit: 255
