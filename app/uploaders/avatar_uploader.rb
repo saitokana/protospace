9f9a760846b0dbdd2d4728dac47f559d91ee95d3
齊藤可奈 <saitokana@saitoukana-no-MacBook-Pro.local>
2015-11-09 19:05:23 +0900

    変更したカラム名に合わせて変更

--- app/uploaders/avatar_uploader.rb
+++ app/uploaders/avatar_uploader.rb
@@ -1,6 +1,6 @@
 # encoding: utf-8
 
-class ImageUploader < CarrierWave::Uploader::Base
+class AvatarUploader < CarrierWave::Uploader::Base
 
   # Include RMagick or MiniMagick support:
   # include CarrierWave::RMagick
@@ -43,7 +43,6 @@ class ImageUploader < CarrierWave::Uploader::Base
   # end
 
   # Override the filename of the uploaded files:
-
   # Avoid using model.id or version_name here, see uploader/store.rb for details.
   # def filename
   #   "something.jpg" if original_filename
