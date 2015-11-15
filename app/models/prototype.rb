73cc1985db24b4134ebb5488669cc21347dfa545
齊藤可奈 <saitokana@saitoukana-no-MacBook-Pro.local>
2015-11-15 18:38:06 +0900

    editに必要なメソッドの定義

--- app/models/prototype.rb
+++ app/models/prototype.rb
@@ -1,5 +1,5 @@
 class Prototype < ActiveRecord::Base
-  belongs_to :users
+  belongs_to :user
   validates_presence_of :user_id, :title, :catch_copy, :concept
   has_many :captured_images
   accepts_nested_attributes_for :captured_images
@@ -8,13 +8,27 @@ class Prototype < ActiveRecord::Base
     captured_images.main.first.name
   end
 
-  def sub_image
-    captured_images.sub
+  def sub_images
+    captured_images.sub.map(&:name)
+  end
+
+  def posted_date
+    created_at.strftime("%b %d")
   end
 
   def create_captured_images(captured_image)
     captured_image.each{ |k, v| k == "main" ? captured_images.main.create(name: v) : captured_images.sub.create(name: v) }
   end
+
+  def destroy_image(prototype)
+    if captured_images.main.count == 2
+      captured_images.main.find_by(prototype_id: prototype.id).destroy
+    end
+    while captured_images.sub.count >= 4 do
+      captured_images.sub.find_by(prototype_id: prototype.id).destroy
+    end
+  end
+
 
   paginates_per 8
 
