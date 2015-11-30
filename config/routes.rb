942eb295b47cdf233f7be8e6924678b709b8fa46
齊藤可奈 <saitokana@saitoukana-no-MacBook-Pro.local>
2015-11-30 21:31:48 +0900

    切り替え用のルーティング作成

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,10 @@ Rails.application.routes.draw do
   devise_for :users
   resources :tags, param: :tag_id, only: [:index, :show]
   resources :users, only: [:show]
+  scope module: :prototypes do
+    resources :newest, only:[:index]
+    resources :popular, only:[:index]
+  end
   resources :prototypes do
     scope module: :prototypes do
       resources :likes, only:[:create, :destroy]
