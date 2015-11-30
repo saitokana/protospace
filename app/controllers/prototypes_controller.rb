43a6beebcf0cb3d3d5cf9b75d871d032af129c46
齊藤可奈 <saitokana@saitoukana-no-MacBook-Pro.local>
2015-11-30 21:29:12 +0900

    デフォルトをnewestの状態に

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -3,7 +3,7 @@ class PrototypesController < ApplicationController
   before_action :set_prototype, only: [:show, :edit, :update]
 
   def index
-    @prototypes = Prototype.all.page(params[:page])
+    @prototypes = Prototype.order('created_at DESC').page(params[:page])
   end
 
   def new
