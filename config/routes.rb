b3abf6d4ff08a924a49092782ec3d4eaf4afc62d
齊藤可奈 <saitokana@saitoukana-no-MacBook-Pro.local>
2015-12-01 01:37:51 +0900

    ルーティングの変更

--- config/routes.rb
+++ config/routes.rb
@@ -2,14 +2,13 @@ Rails.application.routes.draw do
   devise_for :users
   resources :tags, param: :tag_id, only: [:index, :show]
   resources :users, only: [:show]
-  scope module: :prototypes do
-    resources :newest, only:[:index]
-    resources :popular, only:[:index]
-  end
   resources :prototypes do
     scope module: :prototypes do
       resources :likes, only:[:create, :destroy]
       resources :comments, only:[:create]
+    end
+    collection do
+      get :popular
     end
   end
   root to: "prototypes#index"
