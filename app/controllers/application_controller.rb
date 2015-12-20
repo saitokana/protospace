1ddbd6f057ee19f9d14401b7c9c5721c54e2b2ac
齊藤可奈 <saitokana@saitoukana-no-MacBook-Pro.local>
2015-12-20 16:44:38 +0900

    不要カラムの削除

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,8 +6,8 @@ class ApplicationController < ActionController::Base
   add_flash_types :success, :info, :warning, :danger
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.for(:sign_up).push(:profile, :position, :member, :name, :avatar)
-    devise_parameter_sanitizer.for(:account_update).push(:profile, :position, :member, :name, :avatar)
+    devise_parameter_sanitizer.for(:sign_up).push(:name, :avatar)
+    devise_parameter_sanitizer.for(:account_update).push(:name, :avatar)
   end
 
 end
