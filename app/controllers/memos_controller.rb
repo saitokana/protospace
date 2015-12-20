8197322d56356fd73f6fc834c5ebc398435cd11f
齊藤可奈 <saitokana@saitoukana-no-MacBook-Pro.local>
2015-12-21 00:43:33 +0900

    デザイン・アソシエーション

--- app/controllers/memos_controller.rb
+++ app/controllers/memos_controller.rb
@@ -1,22 +1,22 @@
 class MemosController < ApplicationController
 def index
-	@memos = Memo.all
+	@memos = current_user.memos
 	@memo = Memo.new
 end
 
 def create
 	Memo.create(memo_params)
-	@memos = Memo.all
+	@memos = current_user.memos
 end
 
 def destroy
 	Memo.find(params[:id]).destroy
-	@memos = Memo.all
+	@memos = current_user.memos
 end
 
 private
 def memo_params
-	params.require(:memo).permit(:text, :status)
+	params.require(:memo).permit(:text, :status).merge(user_id: current_user.id)
 end
 
 end
